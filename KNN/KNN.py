207a454e5fbd48033d57739c5cbdf932cef8bb1c
Joe <zjqucas@163.com>
2018-04-04 21:34:33 +0800

    restart

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,7 +1,21 @@
 from numpy import *
 import operator
 
-def createDataSet :
-	group = array([[],[],[]])
-	labels = array([])
+def createDataSet():
+	group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+	labels = array(['A','B','C','D'])
 	return group, labels
+
+def classify(inX,dataSet,labels,k):
+	dataSetSize = dataSet.shape[0]
+	diffMat = tile(inX,(dataSetSize,1)) - dataSet
+	sqDiffMat = diffMat**2
+	sqDistances = sqDiffMat.sum(axis=1)
+	distances = sqDistances**0.5
+	sortedDistIndices = distances.argsort()
+	classCount = {}
+	for i in range(k):
+		voteIlabel = labels[sortedDistIndices[i]]
+		classCount[voteIlabel] = classCount.get(voteIlabel,0) +1
+	sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=true)
+	return sortedClassCount[0][0] 
