b3ab6b3fe969c75a395dbe62b932bbdac8439b67
Joe <zjqucas@163.com>
2018-05-28 20:48:24 +0800

    cv learning started

---
+++ python/CV/cvcamera.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import numpy
+import matplotlib as plot
+# 摄像头对象
+cap = cv.VideoCapture(0)
+
+# 显示
+while(1):
+    ret,frame = cap.read()
+    cv.imshow('capture,frame')
+    if(cv.waitKey(1) & 0xFF == ord('q')):
+        break
+cap.release()
+cv.destroyAllWindows()
---
+++ python/CV/cvtest.py
@@ -0,0 +1,54 @@
+# -*- coding : utf-8 -*-
+import cv2 as cv
+
+def test():
+    img = cv.imread('./test.jpg')
+    newImg = img
+    # 设定窗口
+    cv.namedWindow('Image',cv.WINDOW_NORMAL) 
+    cv.imshow('Image',img)
+
+    print(img.shape)
+    print(img.dtype)
+
+    width = img.shape[0]
+    height = img.shape[1]
+    print(width,height)
+    cv.putText(newImg,'hello',(100,100),cv.FONT_HERSHEY_COMPLEX,3,(255,255,255),10)
+    # 等待IO
+    key = cv.waitKey(0)
+    if key == 27:
+        cv.destroyAllWindows()
+    elif key == ord('s'):
+        # 保存图像
+        cv.imwrite('newtest.png',newImg)
+        print("保存完毕!\n")
+    elif key == ord('m'):
+        # 放大图像
+        res = cv.resize(img,(2*width,2*height),interpolation=cv.INTER_CUBIC)
+        cv.imwrite('scaleImg.png',res)
+        print("保存完毕!\n")
+    elif key == ord('b'):
+        # 图像信息二值化
+        th=100
+        ret,binary1=cv.threshold(img,th,255,cv.THRESH_BINARY_INV)
+        ret,binary2=cv.threshold(img,th,255,cv.THRESH_BINARY)
+        ret,binary3=cv.threshold(img,th,255,cv.THRESH_TRUNC)
+        ret,binary4=cv.threshold(img,th,255,cv.THRESH_TOZERO)
+        ret,binary5=cv.threshold(img,th,255,cv.THRESH_TOZERO_INV)
+        #ret,binary6=cv.threshold(img,th,255,cv.THRESH_OTSU)
+        #ret,binary7=cv.threshold(img,th,255,cv.THRESH_TRIANGLE)
+
+        cv.imshow("THRESH_BINARY_INV",binary1)
+        cv.imshow("THRESH_BINARY",binary2)
+        cv.imshow("THRESH_TRUNC",binary3)
+        cv.imshow("THRESH_TOZERO",binary4)
+        cv.imshow("THRESH_TOZERO_INV",binary5)
+        #cv.imshow("THRESH_OTSU",binary6)
+        #cv.imshow("THRESH_TRIANGLE",binary7)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    test()
